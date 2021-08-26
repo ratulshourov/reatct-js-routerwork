1bf56ce9cca9971e64175c1e344cf9ec4ba5ae17
Ratul <ratuljibondhara@gmail.com>
2021-08-26 16:18:24 +0600

    session clear

--- src/Login.js
+++ src/Login.js
@@ -6,11 +6,15 @@ class Login extends Component {
     sessionsave = () => {
         sessionStorage.setItem("username", "ratul")
     }
+    sessiondestroy = () => {
+        sessionStorage.clear();
+    }
     render() {
 
         return (
             <div>
                 <button onClick={this.sessionsave}>Login</button>
+                <button onClick={this.sessiondestroy}>Logout</button>
             </div>
         );
     }
